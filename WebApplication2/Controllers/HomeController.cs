2509226a72ead8d4199223f8d2dfef8946433c29
Arya <eviarya2023@gmail.com>
2023-04-16 23:54:23 +0530

    test

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            string ipAddress = Request.Headers["X-Forwarded-For"];
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                ipAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+
+//            string myValue = "Hello, world!";
+            return Content(ipAddress);
+            //return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
